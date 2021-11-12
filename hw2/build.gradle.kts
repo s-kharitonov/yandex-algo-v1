a12e080dcac13320153758b667eef0756ebe0b34
skharitonov <skharitonov.k@gmail.com>
2021-11-12 13:51:06 +0300

    update gradle and java version

--- hw2/build.gradle.kts
+++ hw2/build.gradle.kts
@@ -1,6 +1,6 @@
 java {
-    sourceCompatibility = JavaVersion.VERSION_16
-    targetCompatibility = JavaVersion.VERSION_16
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.getByName<Test>("test") {
